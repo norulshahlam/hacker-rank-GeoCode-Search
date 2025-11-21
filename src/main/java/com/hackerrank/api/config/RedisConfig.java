ae129662f4b9ecb97de70c083b2446c75a4c2479
norulshahlam <norulshahlam@gmail.com>
2025-11-21 17:51:13 +0800

    first commit

--- src/main/java/com/hackerrank/api/config/RedisConfig.java
+++ src/main/java/com/hackerrank/api/config/RedisConfig.java
@@ -3,9 +3,12 @@ package com.hackerrank.api.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 @EnableRedisRepositories
@@ -19,13 +22,25 @@ public class RedisConfig {
 
     @Bean
     public JedisConnectionFactory jedisConnectionFactory() {
-        //TODO
-        return null;
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+        config.setHostName(redisHostName);
+        config.setPort(redisPort);
+        JedisConnectionFactory factjedisConnectionFactory = new JedisConnectionFactory(config);
+        return factjedisConnectionFactory;
     }
 
     @Bean
     public RedisTemplate<String, Object> redisTemplate() {
-        //TODO
-        return null;
+        RedisTemplate<String, Object> stringObjectRedisTemplate = new RedisTemplate<>();
+        stringObjectRedisTemplate.setConnectionFactory(jedisConnectionFactory());
+        // redis key serializer add
+        stringObjectRedisTemplate.setKeySerializer(new StringRedisSerializer());
+        stringObjectRedisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        // redis value serializer add
+        GenericJackson2JsonRedisSerializer genericJackson2JsonRedisSerializer = new GenericJackson2JsonRedisSerializer();
+        stringObjectRedisTemplate.setValueSerializer(genericJackson2JsonRedisSerializer);
+        stringObjectRedisTemplate.setHashValueSerializer(genericJackson2JsonRedisSerializer);
+        stringObjectRedisTemplate.afterPropertiesSet();
+        return stringObjectRedisTemplate;
     }
 }
--- src/main/java/com/hackerrank/api/repository/GeoCodeRepository.java
+++ src/main/java/com/hackerrank/api/repository/GeoCodeRepository.java
@@ -1,24 +1,66 @@
 package com.hackerrank.api.repository;
 
 import com.hackerrank.api.entity.GeoCode;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 @Repository
 public class GeoCodeRepository {
+
+    private final RedisTemplate<String, Object> redisTemplate;
+
+    @Autowired
+    public GeoCodeRepository(RedisTemplate<String, Object> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
     public GeoCode save(GeoCode geocode) {
-        //TODO
-        return null;
+        if (geocode == null) return null;
+        String redisKey = geocode.getCountry() + "_" + geocode.getCity();
+        // make country_city as key
+        redisTemplate.opsForValue().set(redisKey, geocode);
+        return geocode;
     }
 
     public GeoCode findByCountryAndCity(String country, String city) {
-        //TODO
-        return null;
+        String key = country + "_" + city;
+        Object objectResponse = redisTemplate.opsForValue().get(key);
+        if (objectResponse instanceof GeoCode) {
+            return (GeoCode) objectResponse;
+        }
+        // Handle no result - to return an object with empty code
+        GeoCode emptyResult = new GeoCode();
+        emptyResult.setCountry(country);
+        emptyResult.setCity(city);
+        emptyResult.setCode(objectResponse == null ? "" : String.valueOf(objectResponse));
+        return emptyResult;
     }
 
     public List<GeoCode> findAll() {
-        //TODO
-        return null;
+        Set<String> stringSet = redisTemplate.keys("*_*");
+        List<GeoCode> geoCodeArrayList = new ArrayList<>();
+        if (stringSet == null || stringSet.isEmpty()) return geoCodeArrayList;
+        for (String key : stringSet) {
+            Object val = redisTemplate.opsForValue().get(key);
+            if (val instanceof GeoCode) {
+                geoCodeArrayList.add((GeoCode) val);
+            } else {
+                // map key to country and city
+                int identity = key.indexOf('_');
+                String cntyObject = identity > 0 ? key.substring(0, identity) : key;
+                String ctyObj = identity > 0 ? key.substring(identity + 1) : "";
+                GeoCode newGeoCode = new GeoCode();
+                newGeoCode.setCountry(cntyObject);
+                newGeoCode.setCity(ctyObj);
+                newGeoCode.setCode(val == null ? "" : String.valueOf(val));
+                geoCodeArrayList.add(newGeoCode);
+            }
+        }
+        return geoCodeArrayList;
     }
 }
